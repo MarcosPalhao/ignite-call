3fe9a9ea488897211551764c41587c0b87d84aef
Marcos Vinicius <marcos.palhao18@gmail.com>
2023-01-23 13:26:06 -0300

    feat: calendar

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,12 +1,15 @@
 import { Calendar } from "@/src/components/Calendar";
+import { useState } from "react";
 import { Container, TimePicker, TimePickerHeader, TimePickerItem, TimePickerList } from "./styles";
 
 export function CalendarStep() {
-    const isDateSelected = false
+    const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+
+    const isDateSelected =  !!selectedDate
 
     return (
         <Container isTimePickerOpen={isDateSelected}>
-            <Calendar />
+            <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
 
             {isDateSelected && (
                 <TimePicker>
